7caf5eda55761bf3e5b331ff4ffe21f0c0164643
NekoYo1 <kostyafear203@gmail.com>
2022-11-28 10:59:15 +1000

    Добавьте файлы проекта.

---
+++ MathTaskTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("404f138d-301a-4591-9b62-f7a7a84289fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MathTaskTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTask;
+
+namespace MathTaskTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15retyrned()
+        {
+            // исходные данные 
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            //получаем значения с помощью тестируемого метода
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
